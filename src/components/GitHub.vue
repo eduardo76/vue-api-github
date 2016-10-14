6576ca76410e1b59ba14272b4abee050124315b2
Eduardo Tavares <etavaresf@gmail.com>
2016-10-14 17:36:55 -0300

    Backup

---
+++ src/components/GitHub.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="container">
+        <searchuser 
+            updateUser={this.updateUser}
+            updateRepos={this.updateRepos}
+        />
+        <userinfo 
+            user={this.state.user}
+            repos={this.state.repos}
+        />
+    </div>
+</template>
+
+<script>
+// import SearchUser from './SearchUser'
+// import UserInfo from './UserInfo'
+
+    export default {
+        data () {
+            return {
+                user: null,
+                repos: [],
+            }
+        },
+
+        methods: {
+            updateUser: function(user) {
+                this.setState({user: user});
+            },
+            updateRepos: function(repos) {
+                this.setState({repos: repos});
+            },
+        }
+    }
+</script>
