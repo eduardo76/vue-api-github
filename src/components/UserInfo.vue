449e013ea22f01ea862d3fa8f6aeb77e85af1bb1
Eduardo Tavares <etavaresf@gmail.com>
2020-02-21 21:10:12 -0300

    Update UserInfo.vue
    Alteração concluída

--- src/components/UserInfo.vue
+++ src/components/UserInfo.vue
@@ -4,7 +4,7 @@
             <img class="img-circle" :src="user.avatar_url" alt="avatar" width="140" height="140" />
             <h2>{{user.login}}</h2>
             <p>{{user.name}}</p>
-            <p>Followers: {{user.followers}} / Following: {{user.following}}</p>
+            <p>Seguidores: {{user.followers}} / Following: {{user.following}}</p>
             <p><a class="btn btn-default" :href="user.html_url" role="button">View details</a></p>
         </div>
         <div class="col-lg-8">
